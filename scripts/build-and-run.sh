7479dc865a8943600f8432e8995ad6d6a589e5ca
Philip Davis <philipdavis01@gmail.com>
2022-11-19 04:54:27 +0000

    build script

---
+++ scripts/build-and-run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+branch=$1
+src_dir=$2
+build_dir=$3
+install_dir=$4
+compiler=$5
+fcompiler=$6
+
+git checkout ${branch}
+cd ${build_dir}
+cmake ${src_dir} -DCMAKE_INSTALL_PREFIX=${install_dir} -DENABLE_TESTS=True -DCMAKE_C_COMPILER=${compiler} ${fcompiler}
+make
+make install
+
