e25d706a96dd07c65dc5d934ce0395c29b4123b6
Philip Davis <philipdavis01@gmail.com>
2023-03-08 13:22:18 +0000

    Add the build/run script for docker

---
+++ scripts/build-and-run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+
+branch=$1
+src_dir=$2
+build_dir=$3
+install_dir=$4
+compiler=$5
+fcompiler=$6
+ofi_str=$7
+
+git checkout ${branch}
+cd ${build_dir}
+cmake ${src_dir} -DCMAKE_INSTALL_PREFIX=${install_dir} -DENABLE_TESTS=True -DCMAKE_C_COMPILER=${compiler} -DCMAKE_Fortran_COMPILER=${fcompiler}
+make
+make install
+
+fi_info
+
+cd -
+${install_dir}/bin/dspaces_server ${ofi_str} &
+while [ -f conf.ds ] ; do
+    sleep 1
+done
+sleep 2
+cat conf.ds
+
+wait
