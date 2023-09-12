7f2657d4caf4c6925026aa6225bd0feeb62e404f
Philip Davis <philipdavis01@gmail.com>
2023-09-12 15:00:24 -0400

    Move to src before checkout in build script

--- scripts/build-and-run.sh
+++ scripts/build-and-run.sh
@@ -8,6 +8,7 @@ compiler=$5
 fcompiler=$6
 ofi_str=$7
 
+cd ${src_dir}
 git checkout ${branch}
 cd ${build_dir}
 cmake ${src_dir} -DCMAKE_INSTALL_PREFIX=${install_dir} -DENABLE_TESTS=True -DCMAKE_C_COMPILER=${compiler} -DCMAKE_Fortran_COMPILER=${fcompiler}
