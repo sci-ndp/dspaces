39b5b06cb8cc761d34637ad4a8e6ccf5e2066c59
Philip Davis <philipdavis01@gmail.com>
2024-05-08 09:22:39 -0400

    Release GIL for more dspaces operations in python bindings

--- bindings/python/dspaces_wrapper.c
+++ bindings/python/dspaces_wrapper.c
@@ -22,7 +22,11 @@ PyObject *wrapper_dspaces_init(int rank)
 
     clientp = malloc(sizeof(*clientp));
 
+    // clang-format off
+    Py_BEGIN_ALLOW_THREADS
     ret = dspaces_init(rank, clientp);
+    Py_END_ALLOW_THREADS
+    // clang-format on
     if(ret != 0) {
         sprintf(err_str, "dspaces_init() failed with %i", ret);
         PyErr_SetString(PyExc_RuntimeError, err_str);
@@ -50,7 +54,11 @@ PyObject *wrapper_dspaces_init_mpi(PyObject *commpy)
     }
     clientp = malloc(sizeof(*clientp));
 
+    // clang-format off
+    Py_BEGIN_ALLOW_THREADS
     ret = dspaces_init_mpi(*comm_p, clientp);
+    Py_END_ALLOW_THREADS
+    // clang-format on
     if(ret != 0) {
         sprintf(err_str, "dspaces_init_mpi() failed with %i", ret);
         PyErr_SetString(PyExc_RuntimeError, err_str);
@@ -74,7 +82,11 @@ PyObject *wrapper_dspaces_init_wan(const char *listen_str, const char *conn,
 
     clientp = malloc(sizeof(*clientp));
 
+    // clang-format off
+    Py_BEGIN_ALLOW_THREADS
     ret = dspaces_init_wan(listen_str, conn, rank, clientp);
+    Py_END_ALLOW_THREADS
+    // clang-format on
     if(ret != 0) {
         sprintf(err_str, "dspaces_init_wan() failed with %i", ret);
         PyErr_SetString(PyExc_RuntimeError, err_str);
@@ -103,7 +115,11 @@ PyObject *wrapper_dspaces_init_wan_mpi(const char *listen_str, const char *conn,
     }
     clientp = malloc(sizeof(*clientp));
 
+    // clang-format off
+    Py_BEGIN_ALLOW_THREADS
     ret = dspaces_init_wan_mpi(listen_str, conn, *comm_p, clientp);
+    Py_END_ALLOW_THREADS
+    // clang-format on
     if(ret != 0) {
         sprintf(err_str, "dspaces_init_wan_mpi() failed with %i", ret);
         PyErr_SetString(PyExc_RuntimeError, err_str);
@@ -132,7 +148,11 @@ PyObject *wrapper_dspaces_server_init(const char *listen_str, PyObject *commpy,
     }
     serverp = malloc(sizeof(*serverp));
 
+    // clang-format off
+    Py_BEGIN_ALLOW_THREADS
     ret = dspaces_server_init(listen_str, *comm_p, conf, serverp);
+    Py_END_ALLOW_THREADS
+    // clang-format on
     if(ret != 0) {
         sprintf(err_str, "dspaces_init_mpi() failed with %i", ret);
         PyErr_SetString(PyExc_RuntimeError, err_str);
@@ -190,7 +210,12 @@ void wrapper_dspaces_put(PyObject *clientppy, PyObject *obj, const char *name,
         lb[(ndim-1) - i] = PyLong_AsLong(item);
         ub[(ndim-1) - i] = lb[(ndim-1) - i] + ((long)shape[i] - 1);
     }
+
+    // clang-format off
+    Py_BEGIN_ALLOW_THREADS
     dspaces_put_tag(*clientp, name, version, size, tag, ndim, lb, ub, data);
+    Py_END_ALLOW_THREADS
+    // clang-format on
 
     return;
 }
@@ -219,9 +244,13 @@ PyObject *wrapper_dspaces_get(PyObject *clientppy, const char *name,
         ub[(ndim-1) - i] = PyLong_AsLong(item);
     }
 
-    Py_BEGIN_ALLOW_THREADS dspaces_aget(*clientp, name, version, ndim, lb, ub,
+
+    // clang-format off
+    Py_BEGIN_ALLOW_THREADS 
+    dspaces_aget(*clientp, name, version, ndim, lb, ub,
                                         &data, &tag, timeout);
     Py_END_ALLOW_THREADS
+    // clang-format on
 
         if(data == NULL)
     {
